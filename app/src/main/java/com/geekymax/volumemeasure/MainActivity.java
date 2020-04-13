8edb9f709ae6c30723d7da4bfc6497817a700ef8
huangmengxuan <huangmengxuan@bytedance.com>
2020-04-13 21:44:34 +0800

    feat:增加绘制立方体功能
    Change-Id: Iac21cdf8905804fec0c431e86215ff14195e58ad

--- app/src/main/java/com/geekymax/volumemeasure/MainActivity.java
+++ app/src/main/java/com/geekymax/volumemeasure/MainActivity.java
@@ -44,28 +44,41 @@ import com.google.ar.core.TrackingState;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.ArSceneView;
 import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.math.Quaternion;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.Color;
+import com.google.ar.sceneform.rendering.MaterialFactory;
 import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.ShapeFactory;
+import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import at.markushi.ui.CircleButton;
 
 /**
  * This is an example activity that uses the Sceneform UX package_1 to make common AR tasks easier.
  */
-public class MainActivity extends AppCompatActivity implements OnSceneUpdateListener, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements OnSceneUpdateListener {
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final double MIN_OPENGL_VERSION = 3.0;
 
     private MyArFragment arFragment;
     private ModelRenderable andyRenderable;
+    private ViewRenderable infoRenderable;
     private Session arSession;
     private ArState arState;
 
     // 控件
     private ImageView translucenceDown;
     private CircleButton measureButton;
+
+    private List<Node> vertexList = new ArrayList<>();
 
     @Override
     @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
@@ -94,6 +107,10 @@ public class MainActivity extends AppCompatActivity implements OnSceneUpdateList
                             toast.show();
                             return null;
                         });
+        ViewRenderable.builder()
+                .setView(this, R.layout.ar_info)
+                .build()
+                .thenAccept(renderable -> infoRenderable = renderable);
         arFragment.setOnUpdateListener(this);
         arFragment.setOnTapArPlaneListener(
                 (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
@@ -107,10 +124,14 @@ public class MainActivity extends AppCompatActivity implements OnSceneUpdateList
                     anchorNode.setParent(arFragment.getArSceneView().getScene());
 
                     // Create the transformable andy and add it to the anchor.
-                    TransformableNode andy = new TransformableNode(arFragment.getTransformationSystem());
-                    andy.setParent(anchorNode);
-                    andy.setRenderable(andyRenderable);
-                    andy.select();
+//                    TransformableNode andy = new TransformableNode(arFragment.getTransformationSystem());
+//                    andy.setParent(anchorNode);
+//                    andy.setRenderable(andyRenderable);
+//                    andy.select();
+                    TransformableNode info = new TransformableNode(arFragment.getTransformationSystem());
+                    info.setParent(anchorNode);
+                    info.setRenderable(infoRenderable);
+                    info.select();
                 });
         findWidget();
     }
@@ -180,37 +201,34 @@ public class MainActivity extends AppCompatActivity implements OnSceneUpdateList
         measureButton = findViewById(R.id.measure_btn);
         measureButton.setVisibility(View.INVISIBLE);
         translucenceDown.setVisibility(View.INVISIBLE);
-        measureButton.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        int height = arFragment.getArSceneView().getHeight();
-        int width = arFragment.getArSceneView().getWidth();
-        Log.d(TAG, "onClick: Height,width is " + height + ", " + width);
-        float x = width / 2.0f;
-        float y = height / 2.0f;
-        try {
-            Frame frame = arSession.update();
-            for (HitResult hitResult : frame.hitTest(x, y)) {
-                if (andyRenderable == null) {
-                    return;
+        measureButton.setOnClickListener(v -> {
+            int height = arFragment.getArSceneView().getHeight();
+            int width = arFragment.getArSceneView().getWidth();
+            Log.d(TAG, "onClick: Height,width is " + height + ", " + width);
+            float x = width / 2.0f;
+            float y = height / 2.0f;
+            try {
+                Frame frame = arSession.update();
+                for (HitResult hitResult : frame.hitTest(x, y)) {
+                    if (andyRenderable == null) {
+                        return;
+                    }
+                    // Create the Anchor.
+                    Anchor anchor = hitResult.createAnchor();
+//                    AnchorNode anchorNode = new AnchorNode(anchor);
+//                    anchorNode.setParent(arFragment.getArSceneView().getScene());
+//
+//                    // Create the transformable andy and add it to the anchor.
+//                    TransformableNode andy = new TransformableNode(arFragment.getTransformationSystem());
+//                    andy.setParent(anchorNode);
+//                    andy.setRenderable(andyRenderable);
+//                    andy.select();
+                    drawBox(anchor, 0.2f, 0.4f, 0.3f);
                 }
-                // Create the Anchor.
-                Anchor anchor = hitResult.createAnchor();
-                AnchorNode anchorNode = new AnchorNode(anchor);
-                anchorNode.setParent(arFragment.getArSceneView().getScene());
-
-                // Create the transformable andy and add it to the anchor.
-                TransformableNode andy = new TransformableNode(arFragment.getTransformationSystem());
-                andy.setParent(anchorNode);
-                andy.setRenderable(andyRenderable);
-                andy.select();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        });
 
     }
 
@@ -223,5 +241,69 @@ public class MainActivity extends AppCompatActivity implements OnSceneUpdateList
             measureButton.setVisibility(View.INVISIBLE);
             translucenceDown.setVisibility(View.INVISIBLE);
         }
+    }
+
+    private void drawBox(Anchor startAnchor, float a, float b, float h) {
+        AnchorNode anchorNode = new AnchorNode(startAnchor);
+        anchorNode.setParent(arFragment.getArSceneView().getScene());
+        List<Vector3> locations = new ArrayList<>();
+        for (int i = 0; i <= 1; i++) {
+            for (int j = 0; j <= 1; j++) {
+                for (int k = 0; k <= 1; k++) {
+                    locations.add(Vector3.add(new Vector3(a * i, h * j, k * b), new Vector3(-a / 2, 0, -b / 2)));
+                }
+            }
+        }
+        drawPoint(anchorNode, locations);
+        locations.forEach(v1 -> locations.forEach(v2 -> {
+            Vector3 sub = Vector3.subtract(v1, v2);
+            if (sub.x == 0 && sub.y == 0 && sub.z > 0 || sub.x == 0 && sub.z == 0 && sub.y > 0 || sub.z == 0 && sub.y == 0 && sub.x > 0) {
+                drawLine(anchorNode, v1, v2);
+            }
+        }));
+    }
+
+    private void drawPoint(AnchorNode anchorNode, List<Vector3> locations) {
+        MaterialFactory.makeOpaqueWithColor(this, new Color(0.53f, 0.92f, 0f))
+                .thenAccept(material -> {
+                    ModelRenderable sphere = ShapeFactory.makeSphere(0.01f, new Vector3(0.0f, 0.0f, 0.0f), material);
+                    for (Vector3 loc : locations) {
+                        Node node = new Node();
+                        node.setRenderable(sphere);
+                        node.setParent(anchorNode);
+                        node.setLocalPosition(loc);
+                        vertexList.add(node);
+                    }
+                });
+    }
+
+    private void drawLine(AnchorNode anchorNode, Vector3 firstPosition, Vector3 secondPosition) {
+        MaterialFactory.makeOpaqueWithColor(this, new Color(0.3f, 0.87f, 0f)).thenAccept(material -> {
+            Vector3 difference = Vector3.subtract(firstPosition, secondPosition);
+
+            Vector3 directionFromTopToBottom = difference.normalized();
+            Quaternion rotationFromAToB = Quaternion.lookRotation(directionFromTopToBottom, Vector3.up());
+            ModelRenderable line = ShapeFactory.makeCube(new Vector3(0.01f, 0.01f, difference.length()), Vector3.zero(), material);
+            Node lineNode = new Node();
+            lineNode.setRenderable(line);
+            lineNode.setParent(anchorNode);
+            lineNode.setLocalPosition(Vector3.add(firstPosition, secondPosition).scaled(0.5f));
+            lineNode.setLocalRotation(rotationFromAToB);
+        });
+    }
+
+    private void drawSideArea(AnchorNode anchorNode, Vector3 firstPosition, Vector3 secondPosition, float height) {
+        MaterialFactory.makeOpaqueWithColor(this, new Color(1f, 0, 0)).thenAccept(material -> {
+            Vector3 difference = Vector3.subtract(firstPosition, secondPosition);
+
+            Vector3 directionFromTopToBottom = difference.normalized();
+            Quaternion rotationFromAToB = Quaternion.lookRotation(directionFromTopToBottom, Vector3.up());
+            ModelRenderable line = ShapeFactory.makeCube(new Vector3(0.01f, height, difference.length()), Vector3.zero(), material);
+            Node areaNode = new Node();
+            areaNode.setRenderable(line);
+            areaNode.setParent(anchorNode);
+            areaNode.setLocalPosition(Vector3.add(firstPosition, secondPosition).scaled(0.5f));
+            areaNode.setLocalRotation(rotationFromAToB);
+        });
     }
 }
